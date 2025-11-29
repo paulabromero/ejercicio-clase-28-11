04dbb74cde0f2ac2973e06a0fc20d12e385b2115
Paula Bahamondes R <p1a9u9l8a@gmail.com>
2025-11-28 23:15:52 -0300

    ejercicio realizado en clases falta terminar

---
+++ index.js
@@ -0,0 +1,24 @@
+//crear un arreglo de 4 estudiantes
+
+let nombresEestudiantes = ["Ana", "Pedro", "Juan", "Diego"];
+
+// mostrar la lista utilizando un ciclo for
+for(let index = 0; index < nombresEestudiantes.length; index++){
+    console.log("lista"+index);
+    console.log(nombresEestudiantes [index]);
+}
+
+//contar la cantidad total de estudiantes 
+
+console.log("Cantidad de estudiantes "+ nombresEestudiantes.length);
+
+// 4. agregar 2 estudiantes nuevos al arreglo
+
+nombresEestudiantes.push("Esteban", "Carolina");
+console.log("Nueva Lista");
+console.log(nombresEestudiantes);
+
+// 5. Buscar un estudiante especifico dentro del arreglo
+
+
+// 6. 
